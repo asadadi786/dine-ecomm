a3ab89052637d0613f3a52d7f08a91e89ff0a5b4
asadadi786 <57137231+asadadi786@users.noreply.github.com>
2023-07-13 05:00:12 +0500

    remove export 4m getProductData-vercel deploy err

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { client } from "@/lib/sanityClient";
 import { Image as IImage } from "sanity";
 import { urlForImage } from "../../sanity/lib/image";
 
-export const getProductData = async () => {
+const getProductData = async () => {
   const res = await client.fetch(`*[_type=="product"]{
     _id,
       price,
@@ -27,6 +27,7 @@ interface IProduct {
     name: string;
   };
 }
+
 export default async function Home() {
   const data: IProduct[] = await getProductData();
 
